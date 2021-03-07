2d7ed2db0a739cce4892f4e06e40a7d95e20021e
songjinhoon <jinhoon0319@gmail.com>
2021-03-07 21:46:53 +0900

    2021.03.07 commit

--- src/main/java/org/mddjinhoon/demo/base1/config/auth/CustomOAuth2UserService.java
+++ src/main/java/org/mddjinhoon/demo/base1/config/auth/CustomOAuth2UserService.java
@@ -33,10 +33,12 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         User user = saveOrUpdate(attributes);
         httpSession.setAttribute("user", new SessionUser(user));
 
-        return new DefaultOAuth2User(
+        return null;
+
+        /*return new DefaultOAuth2User(
                 Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
                 attributes.getAttributes(),
-                attributes.getNameAttributeKey());
+                attributes.getNameAttributeKey());*/
     }
 
 
